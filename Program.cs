6dbd0f0ad8a9eefdffe3bb83e578b8d5cc76ed2d
robbfreitas <robertocompromissos@gmail.com>
2022-12-15 14:05:10 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EstruturaDeDadosEmCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Diferenças entre Tipo Referência vs Tipo Valor
+            Point p;
+            p.X = 10;
+            p.Y = 20;
+
+            Console.WriteLine(p);
+
+            
+        }
+    }
+}
